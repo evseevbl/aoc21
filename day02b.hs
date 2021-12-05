5d31f6c9c7b3a7aae0c22781ac5a2731cbddddc3
Boris Evseev <blevseev@avito.ru>
2021-12-05 19:44:10 +0300

    day 2b done

---
+++ day02b.hs
@@ -0,0 +1,31 @@
+import Common
+import Day02
+
+import Data.Maybe
+
+
+-- x, y, aim
+_task02b :: [Move] -> Int -> Int -> Int -> (Int, Int, Int)
+_task02b [] x y a = (x, y, a)
+_task02b moves x y a =
+	let
+		m = head moves
+		(newx, newy, newa) = posDiff m x y a
+	in	_task02b (tail moves) newx newy newa
+
+posDiff :: Move -> Int -> Int -> Int -> (Int, Int, Int)
+posDiff m x y a = case m of
+	(Up, t) -> (x, y, a - t)
+	(Down, t) -> (x, y, a + t)
+	(Forward, t) -> (x+a, y+a*t, a)
+
+task02b :: [Move] -> Int
+task02b moves =
+	let (dx, dy, _) = _task02b moves 0 0 0
+	in dx * dy
+
+main = do
+	vals <- getStrings
+	(putStrLn . show . abs . task02b . mapMaybe parseMove ) vals
+
+
